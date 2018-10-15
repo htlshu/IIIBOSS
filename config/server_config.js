9572247cc94adc45e54b6918797406fe412dfe83
htlshu <285033715@qq.com>
2018-10-15 19:52:58 +0800

    'clone'

---
+++ config/server_config.js
@@ -0,0 +1,6 @@
+const server_config = {
+    livereload : true,
+    port : 8080,
+    host : '10.9.189.43',
+}
+module.exports = server_config;
---
+++ config/webpack_config.js
@@ -0,0 +1,9 @@
+const webpack_config = {
+    entry : {
+
+    },
+    output : {
+        filename : '[name].js'
+    }
+}
+module.exports = webpack_config;
