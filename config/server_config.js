dbd342e6455a85332bdff2450a08a3699e88a7d3
htlshu <285033715@qq.com>
2018-10-16 08:52:23 +0800

    ''

--- config/server_config.js
+++
@@ -1,6 +0,0 @@
-const server_config = {
-    livereload : true,
-    port : 8080,
-    host : '10.9.189.43',
-}
-module.exports = server_config;
--- config/webpack_config.js
+++
@@ -1,9 +0,0 @@
-const webpack_config = {
-    entry : {
-        app : '../src/js/shop_car.js'
-    },
-    output : {
-        filename : '[name].js'
-    }
-}
-module.exports = webpack_config;
